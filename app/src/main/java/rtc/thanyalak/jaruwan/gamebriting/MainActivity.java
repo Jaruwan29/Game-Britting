a05c59322a5ccd3bbd379d025ca399f965e10927
Jaruwan <jaruwanspy1997@gmail.com>
2016-11-18 16:03:18 +0700

    Intent PlayGame

--- app/src/main/java/rtc/thanyalak/jaruwan/gamebriting/MainActivity.java
+++ app/src/main/java/rtc/thanyalak/jaruwan/gamebriting/MainActivity.java
@@ -1,13 +1,43 @@
 package rtc.thanyalak.jaruwan.gamebriting;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ImageView imageView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        imageView = (ImageView) findViewById(R.id.imageView2);
+
+        //ImageView Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_shut);
+
+                mediaPlayer.start();
+
+                startActivity(new Intent(MainActivity.this,PlayGame.class));
+            }
+        });
+
+
+    }   // Main Method
+
+}   // Main Class
